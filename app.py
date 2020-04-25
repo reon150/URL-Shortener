0b7e96325349b370f6d4281e173b010b049a091c
Ronald Ogando Negron <ronaldogando150@gmail.com>
2020-04-24 21:25:54 -0400

    Added shortened

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import random, string
 
 from database import *
 
@@ -10,11 +11,29 @@ db.init_app(app)
 """
 This is to create the database when it doesn't exist yet
 with app.app_context():
-    db.create_all()"""
+    db.create_all()
+"""
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/getShortURL', methods=['POST'])
+def addToBD():
+    original_url = request.form.get('url')
+    short_url = randomString(5)
+    newURL = URLs(original_url=original_url, short_url=short_url)
+    db.session.add(newURL)
+    db.session.commit()
+    return render_template('index.html')
+
+@app.route('/<shortURL>')
+def redirectURL(shortURL):
+    url = URLs.query.filter_by(short_url=shortURL).first()
+    return redirect(url.original_url)
+
+def randomString(length):
+   return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -5,5 +5,5 @@ db = SQLAlchemy()
 class URLs(db.Model):
     __tablename__ = 'url_list'
     id = db.Column(db.Integer, primary_key=True)
-    origial_url = db.Column(db.String(1000))
-    short_url = db.Column(db.String(50))
+    original_url = db.Column(db.String(1000))
+    short_url = db.Column(db.String(50), unique = True)
